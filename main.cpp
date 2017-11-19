fbc5822728954717cf21cc0420a68ff761267a50
jeremy vigil <jeremyvigil@cis.vvc.edu>
2017-11-18 16:56:01 -0800

    update

--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
 /* Filename: main.cpp
  * Developer: Jeremy Vigil
  * Email: Vigilj128906@student.vvc.edu
- * Description: this is a program that reads in a set of countries and prints: 
- *   country with the largest area
- *   country with the largest population
- *   country with thelargest population density(people per square kilometer)
+ * Description: This program gathers input(date, transaction amount, and Description)
+ *   and prints a bank statement.
  */ 
 
 #include <iostream>
 #include <string>
 #include <vector>
 
-
+const int MAX_DAY = 30;
 class Transaction
 {
 public:
@@ -25,23 +23,6 @@ private:
    double amount;
    std::string description;
 };
-
-const int MAX_DAY = 30;
-
-class Statement
-{
-public:
-   Statement();
-   void read();
-   void compute_balances();
-   void print();
-   double get_average_daily_balance();
-   double get_minimum_daily_balance();
-private:
-   std::vector<Transaction> transactions;
-   std::vector<double> daily_balances;
-};
-
 
 Transaction::Transaction()
 {
@@ -63,22 +44,42 @@ int Transaction::get_day()
 {
    
 }
+double Transaction::get_amount()
+{
+  
+}
 void Transaction::print() const
 {
     std::cout << "day:" << day << std::endl;
     std::cout << "amount:" << amount << std::endl;
     std::cout << "description:" << description << std::endl;
 }
-
 void print_transactions(const std::vector<Transaction> t)
 {
     for(int i=0;i<t.size();i++)
         t[i].print();
 }
-
-int main()
+// the Statement class starts here.------------------------------------------.
+class Statement
 {
-   std::vector <Transaction> transaction;
+public:
+   Statement();
+   void read();
+   void compute_balances();
+   void print();
+   double get_average_daily_balance();
+   double get_minimum_daily_balance();
+private:
+   std::vector<Transaction> transactions;
+   std::vector<double> daily_balances;
+};
+Statement::Statement()
+{
+  std::vector<double> daily_balances(30);
+}
+void Statement::read()
+{
+  std::vector <Transaction> transaction;
    bool more = true;
    while(more)
    {
@@ -92,8 +93,22 @@ int main()
        if(response != 'y' && response != 'Y')
          more = false;
    }
-   
+}
+double Statement::get_average_daily_balance(const std::vector<double>& vec)
+{
+    double sum = 0.0;
+    for(int i=0;i < vec.size();i++)
+        sum += vec[i];
+    return sum / vec.size();
+}
+double Statement::get_minimum_daily_balance()
+{
+  
+}
+int main()
+{
+   std::vector<double> v;
    print_transactions(transaction);
-   //stat.print();
+   std::cout << get_average_daily_balance(v)
    return 0;
 }
